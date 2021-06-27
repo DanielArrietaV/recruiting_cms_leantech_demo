c50bdf2647383335fd96ff832cccdf04a9731c19
Daniel <danielarrieta0802@gmail.com>
2021-06-27 18:29:39 -0500

    Add data management interviewer

--- src/pages/TechInterviewer/Components/ListCards/index.js
+++ src/pages/TechInterviewer/Components/ListCards/index.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Card from '../Card'
 import './styles.css'
+import { listOfApplicants } from '../../../../utils/mockData'
 
 function Cards() {
+  const [applicants, setApplicants] = useState([])
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false)
+      setApplicants(listOfApplicants)
+    }, 3000)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  if (loading === true) {
+    return <p>Loading</p>
+  }
+
   return (
     <div className="list_cards_container">
       <ul className="list_Cards">
-        <li>
-          <Card />
-        </li>
-        <li>
-          <Card />
-        </li>
-        <li>
-          <Card />
-        </li>
-        <li>
-          <Card />
-        </li>
-        <li>
-          <Card />
-        </li>
+        {applicants.map((item, index) => {
+          return (
+            <li key={index}>
+              <Card item={item} />
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
